019613c94e5bf1de365d3b8b27e10b62400f88d1
huangkuiwei <1017969390@qq.com>
2025-05-26 23:41:59 +0800

    提交

--- src/pages/dataStatistics/dataStatistics.vue
+++ src/pages/dataStatistics/dataStatistics.vue
@@ -22,10 +22,30 @@
         <text class="highlight" v-if="selectedTime.id === item.id" />
       </view>
     </view>
+
+    <view class="data-charts">
+      <view class="chart-item">
+        <view class="chart-title">热量摄取和运动量</view>
+
+        <view class="chart-box">
+          <l-echart ref="chart1Ref" @finished="init1" />
+        </view>
+      </view>
+
+      <view class="chart-item">
+        <view class="chart-title">热量缺口</view>
+
+        <view class="chart-box">
+          <l-echart ref="chart2Ref" @finished="init2" />
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
+import * as echarts from '@/uni_modules/lime-echart/static/echarts.min';
+
 export default {
   name: 'dataStatistics',
 
@@ -54,6 +74,115 @@ export default {
         },
       ],
       selectedTime: {},
+      option1: {
+        title: {
+          show: false,
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow',
+          },
+        },
+        legend: {
+          bottom: 0,
+        },
+        grid: {
+          top: '5%',
+          left: '0%',
+          right: '0%',
+          bottom: '10%',
+          containLabel: true,
+        },
+        yAxis: {
+          type: 'value',
+          boundaryGap: [0, 0.01],
+        },
+        xAxis: {
+          type: 'category',
+          data: ['05-12', '05-13', '05-14', '05-15', '05-16', '05-17', '05-17'],
+        },
+        series: [
+          {
+            name: '热量摄取',
+            type: 'bar',
+            data: [100, 100, 100, 100, 100, 100, 100],
+            color: '#FFA537',
+          },
+          {
+            name: '运动量',
+            type: 'bar',
+            data: [100, 100, 100, 100, 100, 100, 100],
+            color: '#78F788',
+          },
+        ],
+      },
+      option2: {
+        title: {
+          show: false,
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow',
+          },
+        },
+        legend: {
+          bottom: 0,
+        },
+        grid: {
+          top: '5%',
+          left: '0%',
+          right: '0%',
+          bottom: '10%',
+          containLabel: true,
+        },
+        yAxis: {
+          type: 'value',
+          position: 'top',
+          splitLine: {
+            lineStyle: {
+              type: 'solid',
+            },
+          },
+        },
+        xAxis: {
+          type: 'category',
+          axisLine: {
+            show: false,
+          },
+          axisLabel: {
+            show: false,
+          },
+          axisTick: {
+            show: false,
+          },
+          splitLine: {
+            show: false,
+          },
+          data: ['05-12', '05-13', '05-14', '05-15', '05-16', '05-17', '05-17'],
+        },
+        series: [
+          {
+            type: 'bar',
+            stack: 'Total',
+            label: {
+              show: true,
+              formatter: '{b}',
+            },
+            color: '#78F788',
+            data: [
+              { value: -200 },
+              { value: -300 },
+              { value: 100 },
+              { value: 400 },
+              { value: -150 },
+              { value: 180 },
+              { value: -320 },
+            ],
+          },
+        ],
+      },
     };
   },
 
@@ -64,6 +193,18 @@ export default {
   methods: {
     back() {
       uni.navigateBack();
+    },
+
+    async init1() {
+      // chart 图表实例不能存在data里
+      const chart = await this.$refs.chart1Ref.init(echarts);
+      chart.setOption(this.option1);
+    },
+
+    async init2() {
+      // chart 图表实例不能存在data里
+      const chart = await this.$refs.chart2Ref.init(echarts);
+      chart.setOption(this.option2);
     },
   },
 };
@@ -127,6 +268,27 @@ page {
         height: 12rpx;
         background: #78f788;
         border-radius: 6rpx;
+      }
+    }
+  }
+
+  .data-charts {
+    padding: 40rpx 30rpx;
+
+    .chart-item {
+      margin-bottom: 50rpx;
+
+      .chart-title {
+        font-weight: 500;
+        font-size: 30rpx;
+        color: #1a1a1a;
+        margin-bottom: 28rpx;
+      }
+
+      .chart-box {
+        background: #ffffff;
+        border-radius: 20rpx;
+        padding: 40rpx 20rpx 30rpx;
       }
     }
   }
